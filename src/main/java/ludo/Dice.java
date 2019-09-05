4e1dfe925f9477bad980b8e656330a6b104ffd95
Poojanavgurukul <Pooja17@navgurukul.org>
2019-09-05 11:41:35 +0530

    add.Dice and method too

---
+++ src/main/java/ludo/Dice.java
@@ -0,0 +1,7 @@
+package ludo;
+
+public class Dice {
+    public int getRolled(){
+        return (int)(Math.random()*6+1);
+    }
+}
--- src/main/java/ludo/LudoRunner.java
+++ src/main/java/ludo/LudoRunner.java
@@ -8,9 +8,11 @@ public class LudoRunner {
 
     public static void main(String[] args) {
         List<Token>tokens =new ArrayList<>();
+        Dice dice=new Dice();
         for (int i = 1; i <=4 ; i++) {
             Token token=new Token(i);
             tokens.add(token);
+            System.out.println(dice.getRolled());
         }
         Red red=new Red(tokens);
         Blue blue=new Blue(tokens);

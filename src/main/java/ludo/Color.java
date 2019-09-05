282002d30bcdfe12f24b8ad9b48578fb2bb4fc90
Poojanavgurukul <Pooja17@navgurukul.org>
2019-09-05 10:56:29 +0530

    remove.constructor from the color class

--- src/main/java/ludo/Color.java
+++ src/main/java/ludo/Color.java
@@ -1,12 +1,5 @@
 package ludo;
 
 public enum  Color {
-    RED(0),GREEN(13),BLUE(26),YELLOW(39);
-    private int startingPosition;
-    Color(int startingPosition) {
-        this.startingPosition=startingPosition;
-    }
-    public int getStartingPosition() {
-        return startingPosition;
-    }
+    RED,GREEN,BLUE,YELLOW;
 }
--- src/main/java/ludo/LudoRunner.java
+++ src/main/java/ludo/LudoRunner.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 public class LudoRunner {
     public static void main(String[] args) {
+        // TODO create coins, then pass it to yard while creating
+        // TODO Create a yard and pass it to player
         List<Player> players = Arrays
                 .asList(new Player(Color.RED), new Player(Color.BLUE));
         Game game = new Game(players);

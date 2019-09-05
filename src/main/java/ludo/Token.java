6ff34da431fbb7991f29b46995fbe39c85b292f2
Poojanavgurukul <Pooja17@navgurukul.org>
2019-09-05 11:00:15 +0530

    add.Token class and its constructor

---
+++ src/main/java/ludo/Token.java
@@ -0,0 +1,8 @@
+package ludo;
+
+public class Token {
+    private int id;
+    public Token(int id) {
+        this.id=id;
+    }
+}
---
+++ src/main/java/ludo/Yard.java
@@ -0,0 +1,7 @@
+package ludo;
+
+import java.util.List;
+
+public class Yard {
+    List<Token>tokens;
+}
